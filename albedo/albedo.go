a38263dabdbda4c3a760524eb9e146388473dc14
尘坠 <tout123@126.com>
2021-02-02 13:38:01 +0800

    begin

---
+++ albedo/albedo.go
@@ -0,0 +1,112 @@
+package albedo
+
+import (
+	"crypto/md5"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"math/rand"
+	"strconv"
+	"time"
+)
+
+func MakeUint(num interface{}) uint {
+	switch num.(type) {
+	case int:
+		return uint(num.(int))
+	case uint:
+		return num.(uint)
+	case float32:
+		return uint(num.(float32))
+	case float64:
+		return uint(num.(float64))
+	case string:
+		i, _ := strconv.Atoi(num.(string))
+		return uint(i)
+	default:
+		return 0
+	}
+}
+
+func MakeInt(num interface{}) int {
+	switch num.(type) {
+	case int:
+		return num.(int)
+	default:
+		return int(MakeUint(num))
+	}
+}
+
+func MakeUint32(num interface{}) uint32 {
+	switch num.(type) {
+	case uint32:
+		return num.(uint32)
+	default:
+		return uint32(MakeUint(num))
+	}
+}
+
+func MakeFloat32(num interface{}) float32 {
+	switch num.(type) {
+	case float32:
+		return num.(float32)
+	case float64:
+		return float32(num.(float64))
+	case string:
+		f, _ := strconv.ParseFloat(num.(string), 64)
+		return float32(f)
+	default:
+		return 0
+	}
+}
+
+func MakeString(str interface{}) string {
+	switch str.(type) {
+	case string:
+		return str.(string)
+	case int, uint, int8, uint8, int16, uint16, int32, uint32, int64, uint64:
+		return strconv.Itoa(MakeInt(str))
+	case float32:
+		return strconv.FormatFloat(float64(str.(float32)), 'f', -1, 32)
+	case float64:
+		return strconv.FormatFloat(str.(float64), 'f', -1, 32)
+	default:
+		return ""
+	}
+}
+
+func MakeJson(data interface{}) []byte {
+	b, _ := json.Marshal(data)
+	return b
+}
+
+func MakeStruct(data, res interface{}) {
+	json.Unmarshal(MakeJson(data), &res)
+}
+
+func MakeMap(data string) map[string]interface{} {
+	var m map[string]interface{}
+	json.Unmarshal([]byte(data), &m)
+	return m
+}
+
+func MakeMaps(data string) []map[string]interface{} {
+	var ms []map[string]interface{}
+	json.Unmarshal([]byte(data), &ms)
+	return ms
+}
+
+func MakeUniqId() string {
+	nano := time.Now().UnixNano()
+	sum := md5.Sum([]byte(strconv.FormatInt(nano, 10)))
+	return fmt.Sprintf("%x", sum)
+}
+
+func MakeError(err interface{}) error {
+	return errors.New(MakeString(err))
+}
+
+func MakeSn() string {
+	rand.Seed(time.Now().UnixNano())
+	return time.Now().Format("20060102") + strconv.FormatInt(time.Now().Unix(), 10) + strconv.Itoa(rand.Intn(9999999-1000000)+1000000)
+}
---
+++ keq/keq.go
@@ -0,0 +1,84 @@
+package keq
+
+import (
+	"strconv"
+	"strings"
+	"time"
+)
+
+//日期转换时间戳
+func Timestamp(date string) string {
+	var layout string
+	s := strings.Split(date, " ")
+	if len(s) > 1 {
+		layout = "2006-01-02 15:04:05"
+	} else {
+		s := strings.Split(date, "-")
+		if len(s) > 2 {
+			layout = "2006-01-02"
+		} else {
+			layout = "2006-01"
+		}
+	}
+	location, _ := time.LoadLocation("Local")
+	timestamp, _ := time.ParseInLocation(layout, date, location)
+	return strconv.FormatInt(timestamp.Unix(), 10)
+}
+
+//获取全格式日期
+func Format(timestamp interface{}) string {
+	return Date(timestamp, "2006-01-02 15:04:05")
+}
+
+func Time() string {
+	return strconv.FormatInt(time.Now().Unix(), 10)
+}
+
+//获取格式日期
+func Date(timestamp interface{}, format string) string {
+	sec := int64(makeInt(timestamp))
+	date := time.Unix(sec, 0).Format(format)
+	return date
+}
+
+func Day(days int) string {
+	year, month, day := time.Now().Date()
+	thisMonth := time.Date(year, month, day, 0, 0, 0, 0, time.Local)
+	thisMonth = thisMonth.AddDate(0, 0, days)
+	return thisMonth.Format("2006-01-02")
+}
+
+func Week(weeks int) string {
+	weekDay := int(time.Now().Weekday())
+	if weekDay == 0 {
+		weekDay = 7
+	}
+	timestamp := makeInt(Time()) - (weekDay-1-weeks*7)*24*3600
+	return Date(timestamp, "2006-01-02")
+}
+
+//获取指定月第一天日期
+func Month(months int) string {
+	year, month, _ := time.Now().Date()
+	thisMonth := time.Date(year, month, 1, 0, 0, 0, 0, time.Local)
+	thisMonth = thisMonth.AddDate(0, months, 0)
+	return thisMonth.Format("2006-01-02")
+}
+
+func makeInt(num interface{}) int {
+	switch num.(type) {
+	case int:
+		return num.(int)
+	case uint:
+		return int(num.(uint))
+	case float32:
+		return int(num.(float32))
+	case float64:
+		return int(num.(float64))
+	case string:
+		i, _ := strconv.Atoi(num.(string))
+		return i
+	default:
+		return 0
+	}
+}
